c0f76d993aba422e6b31f3f6c357df9cbac36851
solblack <solbeor@hotmail.com>
2019-04-03 00:16:03 -0300

    fotos de producto, shop-living.php como modelo, y object-fit a imagenes en el CSS

---
+++ floki_php/shop-living.php
@@ -0,0 +1,70 @@
+<html lang="en" dir="ltr">
+
+<head>
+    <?php include("head.php") ?>
+    <title>FLOKI Deco & Design</title>
+
+</head>
+
+<body>
+    <!-- header -->
+    <?php include("header.php") ?>
+    <!-- carrusel 1 -->
+
+<div class="container-fluid" >
+
+<section class="container-fluid">
+<div class="row">
+  <article class="col-lg-3 col-md-4 col-sm-6 col-xs-6 shop-articulo">
+    <img src="images/productos/living/art-1-foto1.jpg" alt="">
+    <p>Mesa de mármol y metal</p>
+    <p>$ precio</p>
+  </article>
+
+  <article class="col-lg-3 col-md-4 col-sm-6 col-xs-6 shop-articulo">
+      <img src="images/productos/living/art-2-foto1.jpg" alt="">
+    <p>Sillón con tela tejida</p>
+    <p>$ precio</p>
+  </article>
+
+  <article class="col-lg-3 col-md-4 col-sm-6 col-xs-6 shop-articulo">
+    <img src="images/productos/living/art-3-foto1.jpg" alt="">
+    <p>Banqueta baja de madera y metal</p>
+    <p>$ precio</p>
+  </article>
+
+  <article class="col-lg-3 col-md-4 col-sm-6 col-xs-6  shop-articulo">
+    <img src="images/productos/living/art-4-foto1.jpg" alt="">
+    <p>Lámpara de pie de metal</p>
+    <p>$ precio</p>
+  </article>
+
+  <article class="col-lg-3 col-md-4 col-sm-6 col-xs-6  shop-articulo">
+    <img src="images/productos/living/art-5-foto1.jpg" alt="">
+    <p>Espejo modelo sol</p>
+    <p>$ precio</p>
+  </article>
+
+  <article class="col-lg-3 col-md-4 col-sm-6 col-xs-6  shop-articulo">
+    <img src="images/productos/living/art-6-foto1.jpg" alt="">
+    <p>Espejo de pie de metal</p>
+    <p>$ precio</p>
+  </article>
+
+</div>
+</section>
+
+<div class="banner-shop"><img class="banner" src="images/productos/banner-shop-floki.jpg" alt="Envio gratis">
+</div>
+
+
+    </div>
+    <!-- FOOTER -->
+    <?php include("footer.php") ?>
+    <!--  scripts de Bootstrap-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
